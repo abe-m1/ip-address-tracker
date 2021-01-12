1c1f3f553c5123666bf34b5137b4db6b947360a2
abe menashy <abestbet1@gmail.com>
2021-01-12 13:11:25 -0800

    set up react

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>IP Address Tracker</p>
+      </header>
+
+      {/* 
+
+Search for any IP address or domain
+
+IP Address
+Location
+Timezone
+  UTC <!-- add offset value dynamically using the API -->
+ISP */}
+      <div class="attribution">
+        Challenge by{' '}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </div>
+  );
+}
+
+export default App;
