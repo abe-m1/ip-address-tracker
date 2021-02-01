0589c685452bb8fc67b3c63ba298ad62678e9988
abe menashy <abestbet1@gmail.com>
2021-02-01 14:36:19 -0800

    connect axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 import Search from './components/Search';
 import Map from './components/Map';
 import Header from './components/Header';
 
 function App() {
+  const [coords, setCoords] = useState({});
+  useEffect(() => {
+    axios
+      .get(
+        'https://geo.ipify.org/api/v1?apiKey=at_bjYUbsHotYNNF7gAMlWD02nZdV8uy&ipAddress=8.8.8.8'
+      )
+      .then((res) => {
+        console.log(res.data.location);
+        // let { lat, lng } = res.data.location;
+        setCoords(res.data.location);
+      });
+  }, []);
   return (
     <div className="App">
       <Header />
       <Search />
-      <Map />
+      <Map coordinates={coords} />
       {/* 
 
 Search for any IP address or domain
